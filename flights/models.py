79f9b69bce3b565faa466ce637c6f50c8714066a
timur <stukantimur811@gmail.com>
2024-08-03 18:05:50 +0300

    created airport model

--- flights/models.py
+++ flights/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Airport(models.Model):
+    name = models.CharField(max_length=255)
+    closest_big_city = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return str(self.name)
