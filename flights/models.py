9ff17b310d1e60522481066614b61ec5900c79bb
timur <stukantimur811@gmail.com>
2024-08-03 18:15:57 +0300

    created airplane model

--- flights/models.py
+++ flights/models.py
@@ -26,3 +26,20 @@ class AirplaneType(models.Model):
 
     def __str__(self) -> str:
         return str(self.name)
+
+
+class Airplane(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+    airplane_type = models.ForeignKey(AirplaneType, on_delete=models.CASCADE)
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+
+
