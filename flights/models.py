534f113e2e805cb404b672ee14a76b19446a05a0
timur <stukantimur811@gmail.com>
2024-08-03 18:08:26 +0300

    created crew model

--- flights/models.py
+++ flights/models.py
@@ -7,3 +7,15 @@ class Airport(models.Model):
 
     def __str__(self) -> str:
         return str(self.name)
+
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    @property
+    def full_name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self) -> str:
+        return self.full_name
