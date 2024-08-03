389f891ea1ed9dc198e8aaebc2e41ea70a908c85
timur <stukantimur811@gmail.com>
2024-08-03 18:36:37 +0300

    Created order model

--- flights/models.py
+++ flights/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 
@@ -60,3 +61,18 @@ class Flight(models.Model):
 
     def __str__(self) -> str:
         return f"{self.route} {self.airplane.name}"
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return str(self.created_at)
+
+    class Meta:
+        ordering = ['-created_at']
+
