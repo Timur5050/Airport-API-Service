0d53d2f299bf3fc23e419152294af4b86e287e2c
timur <stukantimur811@gmail.com>
2024-08-04 22:44:34 +0300

    added jwt tokens support and created url pattern for user registration

---
+++ users/urls.py
@@ -0,0 +1,19 @@
+from users.views import CreateUser
+
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+from rest_framework import routers
+
+
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path("register/", CreateUser.as_view()),
+]
+
+app_name = "users"
