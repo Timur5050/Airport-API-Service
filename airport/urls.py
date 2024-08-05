1beeb907119d2405ce72ae376e472e871fc25631
timur <stukantimur811@gmail.com>
2024-08-05 12:02:59 +0300

    added url pattern for media files

--- airport/urls.py
+++ airport/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
@@ -22,4 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/v1/flights/", include("flights.urls", namespace="flights")),
     path("api/v1/user/", include("users.urls", namespace="users")),
-] + debug_toolbar_urls()
+] + debug_toolbar_urls() + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
