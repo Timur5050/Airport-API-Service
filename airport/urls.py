098a54fc9193f7b2ecc51c74a6fc8b9b17c21f6c
timur <stukantimur811@gmail.com>
2024-08-05 10:36:14 +0300

    added debug toolbar to ulrs

--- airport/urls.py
+++ airport/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/v1/flights/", include("flights.urls", namespace="flights")),
     path("api/v1/user/", include("users.urls", namespace="users")),
-]
+] + debug_toolbar_urls()
