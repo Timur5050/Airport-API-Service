393ffbb68d199db57f10b7914905eacba091263f
timur <stukantimur811@gmail.com>
2024-08-05 12:36:53 +0300

    added url patterns for documentation

--- airport/urls.py
+++ airport/urls.py
@@ -19,9 +19,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/v1/flights/", include("flights.urls", namespace="flights")),
     path("api/v1/user/", include("users.urls", namespace="users")),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
 ] + debug_toolbar_urls() + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
